6d9e9203a21c7cfd63ff4d23b013a6c3ee886c6d
Slekatom <nemo.bma@gmail.com>
2025-04-07 16:46:33 +0300

    Other forum models

--- etelegram/forum/admin.py
+++ etelegram/forum/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import *
 
 admin.site.register(Post)
+admin.site.register(LikePost)
+admin.site.register(DislikePost)
+admin.site.register(Comment)
+admin.site.register(LikeComment)
+admin.site.register(DislikeComment)
--- etelegram/forum/models.py
+++ etelegram/forum/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField(max_length=300)
@@ -8,3 +8,49 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Post: {self.title} -> {self.description}"
+
+
+class LikePost(models.Model):
+    post = models.ForeignKey(Post, related_name="likes", on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, related_name="liked_posts", on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Like by {self.user.username} on post: {self.post.title}"
+
+
+class DislikePost(models.Model):
+    post = models.ForeignKey(Post, related_name="dislikes", on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, related_name="disliked_posts", on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Dislike by {self.user.username} on post: {self.post.title}"
+
+
+class Comment(models.Model):
+    description = models.TextField(max_length=100)
+    user = models.ForeignKey(User, related_name="comments", on_delete=models.DO_NOTHING)
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on post {self.post.title}"
+
+
+class LikeComment(models.Model):
+    comment = models.ForeignKey(Comment, related_name="likes", on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, related_name="liked_comments", on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Like by {self.user.username} on comment: {self.comment.description}"
+
+
+class DislikeComment(models.Model):
+    comment = models.ForeignKey(Comment, related_name="dislikes", on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, related_name="disliked_comments", on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Dislike by {self.user.username} on comment: {self.comment.description}"
