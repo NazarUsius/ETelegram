f981e4944d475be03d98ceeec41bd795518e7739
Slekatom <nemo.bma@gmail.com>
2025-04-08 11:50:24 +0300

    views.py code styling

--- etelegram/forum/views.py
+++ etelegram/forum/views.py
@@ -6,6 +6,30 @@ from .models import *
 def landing_page(request):
     return render(request, 'landing.html')
 
+def get_post_data(post):
+    post_likes = LikePost.objects.filter(post=post)
+    post_dislikes = DislikePost.objects.filter(post=post)
+    post_comments = Comment.objects.filter(post=post)
+
+    comments_data = []
+
+    for comment in post_comments:
+        comment_likes = LikeComment.objects.filter(comment=comment).count()
+        comment_dislikes = DislikeComment.objects.filter(comment=comment).count()
+
+        comments_data.append({
+            'comment': comment,
+            'like_count': comment_likes,
+            'dislike_count': comment_dislikes
+        })
+
+    return {
+        'post': post,
+        'likes': post_likes.count(),
+        'dislikes': post_dislikes.count(),
+        'comments': comments_data
+    }
+
 class PostListView(ListView):
     model = Post
     template_name = "forum.html"
@@ -14,29 +38,11 @@ class PostListView(ListView):
     def get_context_data(self, **kwargs):
         posts = Post.objects.all()
 
-        posts_data = []
+        # Using get_post_data to clean up code repetition
+        posts_data = [get_post_data(post) for post in posts]
 
-        for post in posts:
-            post_likes = LikePost.objects.filter(post=post)
-            post_dislikes = DislikePost.objects.filter(post=post)
-            post_comments = Comment.objects.filter(post=post)
-
-            comment_likes = []
-            comment_dislikes = []
-
-            for comment in post_comments:
-                comment_likes.append(LikeComment.objects.filter(comment=comment).count())
-                comment_dislikes.append(DislikeComment.objects.filter(comment=comment).count())
-
-            posts_data.append({
-                'post': post,
-                'likes': post_likes.count(),
-                'dislikes': post_dislikes.count(),
-                'comments': post_comments,
-                'comment_likes': comment_likes,
-                'comment_dislikes': comment_dislikes
-            })
         return {"posts_data": posts_data}
+
 
 class PostDetailView(DetailView):
     model = Post
@@ -46,29 +52,4 @@ class PostDetailView(DetailView):
     def get_context_data(self, **kwargs):
         post = self.get_object()
 
-        post_likes = LikePost.objects.filter(post=post)
-        post_dislikes = DislikePost.objects.filter(post=post)
-        post_comments = Comment.objects.filter(post=post)
-
-        comments_data = []
-
-        for comment in post_comments:
-            comment_likes = LikeComment.objects.filter(comment=comment).count()
-            comment_dislikes = DislikeComment.objects.filter(comment=comment).count()
-
-            comments_data.append({
-                'comment': comment,
-                'like_count': comment_likes,
-                'dislike_count': comment_dislikes
-            })
-
-        context = {
-            "data": {
-                'post': post,
-                'likes': post_likes.count(),
-                'dislikes': post_dislikes.count(),
-                'comments': comments_data
-            }
-        }
-
-        return context
+        return {'data': get_post_data(post)}
