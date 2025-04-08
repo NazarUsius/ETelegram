5cacbb395340a3191e1c6600e3dde96d1f60a1da
Slekatom <nemo.bma@gmail.com>
2025-04-08 11:30:05 +0300

    detail view

--- etelegram/forum/urls.py
+++ etelegram/forum/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path("", views.forum_main, name = 'forum_main')
+    path("", PostListView.as_view(), name = 'post_list'),
+    path("<int:pk>/", PostDetailView.as_view(), name = "post_detail"),
 ]
--- etelegram/forum/views.py
+++ etelegram/forum/views.py
@@ -1,36 +1,74 @@
 from django.shortcuts import render
-from django.views.generic.edit import FormView
+from django.views.generic import DetailView, ListView
 from .models import *
 
 
 def landing_page(request):
     return render(request, 'landing.html')
 
-def forum_main(request):
-    posts = Post.objects.all()
+class PostListView(ListView):
+    model = Post
+    template_name = "forum.html"
+    context_object_name = "posts_data"
 
-    posts_data = []
+    def get_context_data(self, **kwargs):
+        posts = Post.objects.all()
 
-    for post in posts:
+        posts_data = []
+
+        for post in posts:
+            post_likes = LikePost.objects.filter(post=post)
+            post_dislikes = DislikePost.objects.filter(post=post)
+            post_comments = Comment.objects.filter(post=post)
+
+            comment_likes = []
+            comment_dislikes = []
+
+            for comment in post_comments:
+                comment_likes.append(LikeComment.objects.filter(comment=comment).count())
+                comment_dislikes.append(DislikeComment.objects.filter(comment=comment).count())
+
+            posts_data.append({
+                'post': post,
+                'likes': post_likes.count(),
+                'dislikes': post_dislikes.count(),
+                'comments': post_comments,
+                'comment_likes': comment_likes,
+                'comment_dislikes': comment_dislikes
+            })
+        return {"posts_data": posts_data}
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    context_object_name = "data"
+
+    def get_context_data(self, **kwargs):
+        post = self.get_object()
 
         post_likes = LikePost.objects.filter(post=post)
         post_dislikes = DislikePost.objects.filter(post=post)
         post_comments = Comment.objects.filter(post=post)
 
-        comment_likes = []
-        comment_dislikes = []
+        comments_data = []
 
         for comment in post_comments:
-            comment_likes.append(LikeComment.objects.filter(comment=comment).count())
-            comment_dislikes.append(DislikeComment.objects.filter(comment=comment).count())
+            comment_likes = LikeComment.objects.filter(comment=comment).count()
+            comment_dislikes = DislikeComment.objects.filter(comment=comment).count()
 
-        posts_data.append({
-            'post': post,
-            'likes': post_likes.count(),
-            'dislikes': post_dislikes.count(),
-            'comments': post_comments,
-            'comment_likes': comment_likes,
-            'comment_dislikes': comment_dislikes
-        })
+            comments_data.append({
+                'comment': comment,
+                'like_count': comment_likes,
+                'dislike_count': comment_dislikes
+            })
 
-    return render(request, "forum.html", {"posts_data": posts_data})
+        context = {
+            "data": {
+                'post': post,
+                'likes': post_likes.count(),
+                'dislikes': post_dislikes.count(),
+                'comments': comments_data
+            }
+        }
+
+        return context
