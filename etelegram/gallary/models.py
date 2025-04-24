5f2d7f8b26b1809f0545b5abab1f9195c63b2e26
NazarUsius <nazar.gadzhalov@gmail.com>
2025-04-24 21:17:28 +0300

    Added model for galery

--- etelegram/gallary/models.py
+++ etelegram/gallary/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Media(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    media = models.FileField(upload_to='galary/')
