7ae595e1bf8b9f7d3d4702aeeb96703f376ead38
NazarUsius <nazar.gadzhalov@gmail.com>
2025-04-24 20:58:52 +0300

    Fixed bug with register

--- etelegram/accounts/forms.py
+++ etelegram/accounts/forms.py
@@ -5,8 +5,9 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ("username", "password", "email", "avatar", "gender", "birth_date")
+        fields = ("username", "email", "avatar", "gender", "birth_date")
         widgets = {
             'birth_date': forms.DateInput(attrs={'type': 'date'}),
         }
+
 
--- etelegram/accounts/views.py
+++ etelegram/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 from .forms import CustomUserCreationForm
+from django.contrib import messages
 
 User = get_user_model()
 
@@ -8,9 +9,18 @@ def register_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('login')
+            user = form.save()
+            login(request, user)
+            if request.user.is_authenticated:
+                messages.success(request, 'Вы успешно зарегистрировались!')
+                return redirect('index')
+            else:
+                messages.error(request, 'Ошибка при авторизации!')
+                return redirect('login')
+        else:
+            messages.error(request, 'Пожалуйста, исправьте ошибки в форме.')
     else:
         form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
 
