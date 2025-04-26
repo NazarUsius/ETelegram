16b15a4206fee60db1e41128c7b8ff4e4569fc06
Slekatom <nemo.bma@gmail.com>
2025-04-26 21:08:54 +0300

    Models for voting

---
+++ etelegram/voting/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.conf import settings
+
+
+class Voting(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=70, blank = True, null = True)
+    rating = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Voting: {self.title}"
+
+class Answer(models.Model):
+    title = models.CharField(max_length=30)
+
+class UserAnswer(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
+class Session(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    voting = models.ForeignKey(Voting, on_delete=models.CASCADE)
+
+
