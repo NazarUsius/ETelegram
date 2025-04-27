48bbe054001303eba0d6c9bf8e503f4fe3224a15
NazarUsius <nazar.gadzhalov@gmail.com>
2025-04-27 13:38:17 +0300

    Added profile view by user id

--- etelegram/profilemenu/urls.py
+++ etelegram/profilemenu/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import profile_view, edit_profile_view
 
 urlpatterns = [
-    path('', profile_view, name='profile'),
+    path('<int:id>', profile_view, name='profile'),
     path('edit/', edit_profile_view, name='edit_profile'),
 ]
--- etelegram/profilemenu/views.py
+++ etelegram/profilemenu/views.py
@@ -1,14 +1,23 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import UserProfileForm
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+
+
+
+User = get_user_model()
+
 
 
 @login_required
-def profile_view(request):
-    user = request.user
+def profile_view(request, id):
+    user = get_object_or_404(User, pk=id)
     return render(request, 'profile/profile.html', {
         'user': user
     })
+
+
 
 @login_required
 def edit_profile_view(request):
