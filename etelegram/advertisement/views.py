66acc0a763129a6b26aaf0f4aa1d255f05e85242
Slekatom <nemo.bma@gmail.com>
2025-05-03 00:54:08 +0300

    views for advertisements

--- etelegram/advertisement/views.py
+++ etelegram/advertisement/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from .models import Advertisement
+from .forms import AddCreate, ApproveAddCreate
+from django.urls import reverse_lazy
 
-# Create your views here.
+class AddListView(ListView):
+    model = Advertisement
+    template_name = "advertisement/advertisement_list.html"
+    context_object_name = "data"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["approved_advertisements"] = Advertisement.objects.filter(status="Approved")
+        return context
+
+class AddCreateView(CreateView):
+    model = Advertisement
+    template_name = "advertisement/advertisement_create.html"
+    form_class = AddCreate
+    success_url = reverse_lazy('add_list')
+
+    def form_valid(self, form):
+        form.instance.status = "In queue"
+        return super().form_valid(form)
+
+class ApproveListView(ListView):
+    model = Advertisement
+    template_name = "advertisement/in_queue_advertisement_list.html"
+    context_object_name = "data"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["approved_advertisements"] = Advertisement.objects.filter(status="In queue")
+        return context
+
+class ApproveCreateView(CreateView):
+    model = Advertisement
+    template_name = "advertisement/advertisement_create.html"
+    form_class = ApproveAddCreate
+    success_url = reverse_lazy('approve_list')
+
+    def form_valid(self, form):
+        form.instance.status = "Approved"
+        return super().form_valid(form)
